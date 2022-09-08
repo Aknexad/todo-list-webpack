b91b99d4cbc9a44a59ae2b09ada7814d460f1af4
Aknexad <aknejad@outlook.com>
2022-09-08 12:59:09 +0430

    added function to generate id

--- src/data.js
+++ src/data.js
@@ -1,8 +1,11 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const taskData = [
   {
     title: 'testing task',
     date: Date.now(),
     detail: 'this is a dome text for testing',
+    id: '01',
   },
 ];
 
@@ -10,7 +13,13 @@ const listData = [
   {
     title: 'testing list',
     tasks: [taskData[0]],
+    id: '01',
   },
 ];
 
-export { taskData, listData };
+function minUuid() {
+  let id = uuidv4();
+  return id.substring(0, 6);
+}
+
+export { taskData, listData, minUuid };
